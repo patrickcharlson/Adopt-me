274c0437b23ffa79d22c483c63662c67c99e22d5
patrickcharlson <patrickcharlson@gmail.com>
2022-09-28 17:05:07 +0300

    second commit

---
+++ src/components/Pet.js
@@ -0,0 +1,24 @@
+import { Link } from "@reach/router";
+import React from "react";
+
+
+export default class Pet extends React.Component {
+  render() {
+    const { name, animal, breed, media, location, id } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
+    return (
+      <Link className="pet" to={`/details/${id}`}>
+        <div className="image-container">
+          <img src={photos[0].value} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        </div>
+      </Link>
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
 
 module.exports = function(_env, argv) {
   const isProduction = argv.mode === "production";
@@ -16,12 +18,28 @@ module.exports = function(_env, argv) {
     resolve: {
       extensions: [".js", ".jsx", ".json"]
     },
+    devServer: {
+      port: 3000,
+      compress: true,
+      historyApiFallback: true,
+      open: true,
+      client: {
+        overlay: true
+      }
+    },
     module: {
       rules: [
         {
           test: /\.(jsx?)$/,
           loader: "babel-loader",
           exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader"
+          ]
         }
       ]
     },
@@ -29,7 +47,14 @@ module.exports = function(_env, argv) {
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public/index.html"),
         inject: true
+      }),
+      isProduction && new MiniCssExtractPlugin({
+        filename: "assets/css/[name].[contenthash:8].css",
+        chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
+      }),
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(isProduction ? "production" : "development")
       })
-    ]
+    ].filter(Boolean)
   };
 };
